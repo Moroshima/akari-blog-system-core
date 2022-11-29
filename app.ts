c0ba68f75a94405604b54bbafd42f1aa61c3d8eb
Moroshima <2524332942aliez@gmail.com>
2022-11-29 21:51:32 +0800

    initial commit

---
+++ app.ts
@@ -0,0 +1,27 @@
+const Koa = require("koa");
+const app = new Koa();
+const port = 8080;
+
+// logger
+app.use(async (ctx: any, next: any) => {
+  await next();
+  const rt = ctx.response.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+// x-response-time
+app.use(async (ctx: any, next: any) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set("X-Response-Time", `${ms}ms`);
+});
+
+// response
+app.use(async (ctx: any) => {
+  ctx.response.body = "Hello World";
+  ctx.response.status = 201;
+});
+
+console.log(`HTTP webserver running. Access it at: http://localhost:${port}/`);
+app.listen(port);
